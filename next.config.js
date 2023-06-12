69f1f7e455a92d0eb87acbf4f2c5809c6efaf877
Damon Windsor <damonwindsor@gmail.com>
2023-06-13 01:18:49 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -44,7 +44,7 @@ module.exports = (phase) => {
   const env = {
     SERVER_NAME: (() => {
       if (isDev) return 'http://localhost:3000/'
-      if (isProd) return 'https://exceptional-vpn.vercel.app/'
+      if (isProd) return 'https://exceptional-vpn-damonwindsor97.vercel.app/'
     })(),
     NEWS_API_KEY: process.env.NEWS_API_KEY,
     NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID,
