607b59b20b08c539d165ddfdf8598fc0e61cc6c9
Damon Windsor <damonwindsor@gmail.com>
2023-06-13 01:12:39 +1000

    Update news.jsx

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -2,47 +2,51 @@ import { Fragment } from 'react'
 import ArticleList from '@/components/features/articles/ArticleList/ArticleList';
 
 const newsPage = (props) => {
-  const { vpnArticles } = props;
+    const { vpnArticles } = props;
 
-  return (
-    <Fragment>
-      <h1>News</h1>
-      <h4>The Latest in VPN News</h4>
-      {/* Check if vpnArticles is undefined */}
-      {vpnArticles ? (
-        vpnArticles.length > 0 ? (
+    if (vpnArticles === null) {
+      return (
+        <div>
+          <p>Failed to fetch VPN articles.</p>
+          <p>Please try again later.</p>
+        </div>
+      );
+    }
+  
+    return (
+      <Fragment>
+        <h1>News</h1>
+        <h4>The Latest in VPN News</h4>
+        {vpnArticles && vpnArticles.length > 0 ? (
           <ArticleList articles={vpnArticles} />
         ) : (
           <p>No articles available</p>
-        )
-      ) : (
-        <p>Loading...</p>
-      )}
-    </Fragment>
-  )
+        )}
+      </Fragment>
+    );
 }
 
 export const getStaticProps = async () => {
-  try {
-    const response = await fetch(`https://newsapi.org/v2/everything?sources=abc-news-au&pageSize=4&apiKey=${process.env.NEWS_API_KEY}`);
-    const data = await response.json();
-    const articles = data.articles;
-
-    return {
-      props: {
-        vpnArticles: articles,
-      },
-      revalidate: 60
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      props: {
-        vpnArticles: null,
-      },
-      revalidate: 60
-    };
-  }
-};
+    try {
+      const response = await fetch(`https://newsapi.org/v2/everything?sources=abc-news-au&pageSize=4&apiKey=${process.env.NEWS_API_KEY}`);
+      const data = await response.json();
+      const articles = data.articles || null;
+  
+      return {
+        props: {
+          vpnArticles: articles,
+        },
+        revalidate: 60
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        props: {
+          vpnArticles: null,
+        },
+        revalidate: 60
+      };
+    }
+  };
 
 export default newsPage;
